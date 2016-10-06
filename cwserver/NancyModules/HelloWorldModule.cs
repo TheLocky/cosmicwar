32a51195f4234c38d1c05150b4653a5fea54521c
Виктор <xtpvxt@gmail.com>
2016-10-06 12:16:52 +1000

    nancy + asp.net server init

---
+++ cwserver/NancyModules/HelloWorldModule.cs
@@ -0,0 +1,12 @@
+﻿using Nancy;
+
+namespace cwserver.NancyModules
+{
+    public class HelloWorldModule : NancyModule
+    {
+        public HelloWorldModule()
+        {
+            Get["/"] = x => "<p>Hello World</p>";
+        }
+    }
+}
