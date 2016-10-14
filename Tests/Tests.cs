d9b1c429181b66c7455d39fd132cc96aa6339db4
Mihail Babich <bch@feip.co>
2016-10-14 23:09:10 +1000

    Tests: add UnitTests

---
+++ Tests/Tests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests {
+    [TestClass]
+    public class Tests {
+
+        [TestMethod]
+        public void True() {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void False() {
+            Assert.IsFalse(false);
+        }
+
+    }
+}
